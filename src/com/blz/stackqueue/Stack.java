c0fae42e1f95121e6c59c95c5c409d4b0e2c9204
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-21 23:14:56 +0530

    Added: Ability to create a stack

---
+++ src/com/blz/stackqueue/Stack.java
@@ -0,0 +1,24 @@
+package com.blz.stackqueue;
+
+public class Stack {
+		
+	LinkedList list = new LinkedList();
+
+	public void push(Object data) {
+		list.addLast(data);
+	}
+
+	public void display() {
+		list.display();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("welcome to stack program");
+
+		Stack stack = new Stack();
+		stack.push(56);
+		stack.push(30);
+		stack.push(70);
+		stack.display();
+	}
+}
