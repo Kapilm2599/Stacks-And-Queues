f84f3e9649bc78e3f28c6482ae92bf9365567e01
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-22 20:14:06 +0530

    Added: Ability to create a queue

--- src/com/blz/stackqueue/LinkedList.java
+++ src/com/blz/stackqueue/LinkedList.java
@@ -1,14 +1,20 @@
 package com.blz.stackqueue;
 
 public class LinkedList {
-	Node head;
+	Node head; 
 
 	class Node {
 		Object data;
-		Node ref; 
+		Node ref;
 		Node(Object data) {
 			this.data = data;
 		}
+	}
+
+	public void addFirst(Object data) {
+		Node newNode = new Node(data);
+		newNode.ref = head;
+		head = newNode;
 	}
 
 	public void addLast(Object data) {
@@ -25,16 +31,16 @@ public class LinkedList {
 			temp.ref = newNode;
 		}
 	}
-	
+
 	public void deleteFirst() {
 		head = head.ref;
 	}
 
 	public void display() {
-		Node temp = head;
 		if (head == null)
-			System.out.println("No elements to show.");
+			System.out.println("No elements in list");
 		else {
+			Node temp = head;
 			while (temp != null) {
 				if (temp.ref != null)
 					System.out.print(temp.data + " > ");
@@ -46,4 +52,55 @@ public class LinkedList {
 		}
 	}
 
+	public void deleteLast() {
+		if (head == null)
+			System.out.println("List is Empty");
+		else if (head.ref == null)
+			head = null;
+		else {
+			Node temp = head;
+			while (temp.ref.ref != null) {
+				temp = temp.ref;
+			}
+			temp.ref = null;
+		}
+	}
+
+	public Object searchData(Object input) {
+		Node temp = head;
+		while (temp != null) {
+			if (temp.data == input) {
+				return temp.data;
+			}
+
+			temp = temp.ref;
+		}
+		return null;
+	}
+
+	public void addNodeAtPosition(Object data, int position) {
+		Node newNode = new Node(data);
+		int index = 0;
+		Node left = head;
+		Node right = left.ref;
+		while (index < position) { 
+			index++;
+			left = left.ref;
+			right = right.ref;
+		}
+		newNode.ref = right;
+		left.ref = newNode;
+	}
+
+	public void deleteNodeAtPosition(int position) {
+		int index = 0;
+		Node left = head;
+		Node right = left.ref;
+		while (index < position - 1) {
+			left = left.ref;
+			right = right.ref;
+			index++;
+		}
+		left.ref = right.ref;
+	}
 }
