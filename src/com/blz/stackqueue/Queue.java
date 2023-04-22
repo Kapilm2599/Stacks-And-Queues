0b8c820206975e5bd5b650af5406f95e38cb46fa
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-22 20:30:54 +0530

    Added: Ability to dequeue element from the queue

--- src/com/blz/stackqueue/Queue.java
+++ src/com/blz/stackqueue/Queue.java
@@ -7,17 +7,28 @@ public class Queue {
 	public void enQueue(Object data) {
 		list.addFirst(data);
 	}
+	
+	public void deQueue() {
+		list.deleteFirst();
+	}
 
 	public void display() {
 		list.display();
 	}
 
 	public static void main(String[] args) {
-		System.out.println("welcome to Queue program");
+		System.out.println("welcome to Stack and Queue Data Structure");
 		Queue queue = new Queue();
 		queue.enQueue(70);
 		queue.enQueue(30);
 		queue.enQueue(56);
+		queue.display();
+		queue.display();
+		queue.deQueue();
+		queue.display();
+		queue.deQueue();
+		queue.display();
+		queue.deQueue();
 		queue.display();
 	}
 
